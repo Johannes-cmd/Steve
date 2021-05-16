d079fcb71ccf53f468c712842d7fbd922f576361
Johannes-cmd <johannes@sventus.com>
2021-05-15 21:53:42 -0300

    Esp32 version with better joystick

--- Steve 2.4/Steve_2.4_testings.ino
+++ Steve 2.4/Steve_2.4_testings.ino
@@ -8,16 +8,22 @@
 #include <RemoteMeSocketConnector.h>
 #include <RemoteMeDirectWebSocketConnector.h> // !important library needs WebSockets by Markus Sattler Please install it from Library manager
 
-#include <ESP8266WiFi.h>
+#include <WiFi.h>
 
 
-#define ENA D3
-#define IN1 D1
-#define IN2 D2
+const int ENA = 16; //D3
+#define IN1 17//D1
+#define IN2 18//D2
 
-#define IN3 D5
-#define IN4 D6
-#define ENB D7
+#define IN3 19//D5
+#define IN4 21//D6
+const int ENB = 22; //D7
+
+// setting PWM properties
+const int freq = 10000;
+const int speedmA = 0;
+const int speedmB = 0;
+const int resolution = 8;
 
 //pra frente
 int a = 0;
@@ -35,8 +41,6 @@ unsigned long lastExecutedMillis1 = 0;
 unsigned long lastExecutedMillis2 = 0;
 unsigned long lastExecutedMillis3 = 0;
 
-RemoteMe & remoteMe = RemoteMe::getInstance(TOKEN, DEVICE_ID);
-
 int vertical;
 int horizontal;
 int X;
@@ -45,38 +49,24 @@ int Y;
 int motorA;
 int motorB;
 
-//***** CODE FOR COMFORTABLE VARIABLE SET *******
+RemoteMe& remoteMe = RemoteMe::getInstance(TOKEN, DEVICE_ID);
 
-inline void setJoystick(int16_t i1, int16_t i2) {
-  remoteMe.getVariables() -> setSmallInteger2("Joystick", i1, i2);
-}
+//*************** CODE FOR COMFORTABLE VARIABLE SET *********************
 
-inline void setBackward(boolean b) {
-  remoteMe.getVariables() -> setBoolean("Backward", b);
-}
-inline void setForward(boolean b) {
-  remoteMe.getVariables() -> setBoolean("Forward", b);
-}
-inline void setLeft(boolean b) {
-  remoteMe.getVariables() -> setBoolean("Left", b);
-}
-inline void setRight(boolean b) {
-  remoteMe.getVariables() -> setBoolean("Right", b);
-}
-inline void setSpeed(int32_t i) {
-  remoteMe.getVariables() -> setInteger("Speed", i);
-}
-inline void setWiFi_strength(int32_t i) {
-  remoteMe.getVariables() -> setInteger("WiFi strength", i);
-}
+inline void setForward(boolean b) {remoteMe.getVariables()->setBoolean("Forward", b); }
+inline void setBackward(boolean b) {remoteMe.getVariables()->setBoolean("Backward", b); }
+inline void setLeft(boolean b) {remoteMe.getVariables()->setBoolean("Left", b); }
+inline void setRight(boolean b) {remoteMe.getVariables()->setBoolean("Right", b); }
+inline void setSpeed(int32_t i) {remoteMe.getVariables()->setInteger("Speed", i); }
+inline void setJoystick(int16_t i1, int16_t i2) {remoteMe.getVariables()->setSmallInteger2("Joystick", i1, i2); }
+inline void setWiFi_strength(int32_t i) {remoteMe.getVariables()->setInteger("WiFi strength", i); }
 
-//***** IMPLEMENT FUNCTIONS BELOW *******
+//*************** IMPLEMENT FUNCTIONS BELOW *********************
 
 void onWiFi_strengthChange(int32_t i) {
 }
 
 void onJoystickChange(int16_t i1, int16_t i2) {
-  //Serial.printf("onJoystickChange: i1: %d, i2: %d\n",i1, i2);
 
   vertical = i2;
   horizontal = i1;
@@ -86,9 +76,9 @@ void onJoystickChange(int16_t i1, int16_t i2) {
 
   //as duas rodas para frente:
 
-  if (i2 >= 13) {   
+  if (i2 >= 3) {   
 
-    if (i1 >= 13) {    //se para direita
+    if (i1 >= 3) {    //se para direita
       if (i1 * 2 <= i2) {
         
       digitalWrite(IN1, HIGH);
@@ -97,8 +87,8 @@ void onJoystickChange(int16_t i1, int16_t i2) {
       digitalWrite(IN3, LOW);
       digitalWrite(IN4, HIGH);
 
-      analogWrite(ENB, i2);
-      analogWrite(ENA, i2);
+      ledcWrite(speedmB, i2);
+     ledcWrite(speedmA, i2);
 
       Serial.print("frente");
       Serial.println(i2);
@@ -112,8 +102,8 @@ void onJoystickChange(int16_t i1, int16_t i2) {
         digitalWrite(IN3, LOW);
         digitalWrite(IN4, HIGH);
 
-        analogWrite(ENB, i1);
-        analogWrite(ENA, i1);
+        ledcWrite(speedmB, i1);
+        ledcWrite(speedmA, i1);
 
         Serial.println("direita");
       }
@@ -128,12 +118,12 @@ void onJoystickChange(int16_t i1, int16_t i2) {
 
         motorB = i2 + i1;             // change later to if one is bigger, chose that one...
         motorA = motorB * 3 / 4;
-        if (motorB >= 1023) {
-          motorB = 1023;
+        if (motorB >= 255) {
+          motorB = 255;
         }
         Serial.println("frente direita");
-        analogWrite(ENB, motorA);
-        analogWrite(ENA, motorB);
+        ledcWrite(speedmB, motorA);
+        ledcWrite(speedmA, motorB);
       }
     }
 
@@ -147,8 +137,8 @@ void onJoystickChange(int16_t i1, int16_t i2) {
       digitalWrite(IN3, LOW);
       digitalWrite(IN4, HIGH);
 
-      analogWrite(ENB, i2);
-      analogWrite(ENA, i2);
+      ledcWrite(speedmB, i2);
+      ledcWrite(speedmA, i2);
 
       Serial.print("frente");
       Serial.println(i2);
@@ -164,8 +154,8 @@ void onJoystickChange(int16_t i1, int16_t i2) {
 
         motorB = -horizontal;
 
-        analogWrite(ENB, motorB);
-        analogWrite(ENA, motorB);
+        ledcWrite(speedmB, motorB);
+        ledcWrite(speedmA, motorB);
         
         Serial.println("esquerda");
       }
@@ -180,22 +170,22 @@ void onJoystickChange(int16_t i1, int16_t i2) {
 
         motorA = vertical - horizontal;
         motorB = motorA * 3 / 4;
-        if (motorA >= 1023) {
-          motorA = 1023;
+        if (motorA >= 255) {
+          motorA = 255;
         }
         Serial.println("frente esquerda");
         Serial.println(motorA);
         Serial.println(motorB);
     
-        analogWrite(ENB, motorA);
-        analogWrite(ENA, motorB);
+        ledcWrite(speedmB, motorA);
+        ledcWrite(speedmA, motorB);
       }
     }
   }
 
-  if (i2 <= 13) {           //as duas para traz:
+  if (i2 <= 3) {           //as duas para traz:
       
-    if (i1 <= 13) {     //se para esquerda
+    if (i1 <= 3) {     //se para esquerda
  
       if (-i1 * 2 <= -i2) {
 
@@ -207,8 +197,8 @@ void onJoystickChange(int16_t i1, int16_t i2) {
 
         motorA = -i2;
 
-        analogWrite(ENB, motorA);
-        analogWrite(ENA, motorA);
+        ledcWrite(speedmB, motorA);
+        ledcWrite(speedmA, motorA);
         
         Serial.println("traz");
       }
@@ -223,8 +213,8 @@ void onJoystickChange(int16_t i1, int16_t i2) {
 
         motorB = -horizontal;
 
-        analogWrite(ENB, motorB);
-        analogWrite(ENA, motorB);
+        ledcWrite(speedmB, motorB);
+        ledcWrite(speedmA, motorB);
         
         Serial.println("esquerda");
       }
@@ -239,16 +229,16 @@ void onJoystickChange(int16_t i1, int16_t i2) {
 
         motorB = -i2 - i1;
         motorA = motorB * 3 / 4;
-        if (motorB >= 1023) {
-          motorB = 1023;
+        if (motorB >= 255) {
+          motorB = 255;
         }
-        analogWrite(ENA, motorA);
-        analogWrite(ENB, motorB);
+        ledcWrite(speedmA, motorA);
+        ledcWrite(speedmB, motorB);
         Serial.println("traz esquerda");
       }
     }
     
-     if (i1 >= 13) {    //se para direita
+     if (i1 >= 3) {    //se para direita
 
         if (i1 * 2 <= -i2) {
 
@@ -260,8 +250,8 @@ void onJoystickChange(int16_t i1, int16_t i2) {
 
         motorA = -i2;
 
-        analogWrite(ENB, motorA);
-        analogWrite(ENA, motorA);
+        ledcWrite(speedmB, motorA);
+        ledcWrite(speedmA, motorA);
         
         Serial.println("traz");
       }
@@ -274,8 +264,8 @@ void onJoystickChange(int16_t i1, int16_t i2) {
         digitalWrite(IN3, LOW);
         digitalWrite(IN4, HIGH);
 
-        analogWrite(ENB, i1);
-        analogWrite(ENA, i1);
+        ledcWrite(speedmB, i1);
+        ledcWrite(speedmA, i1);
 
         Serial.println("direita");
       }
@@ -290,11 +280,11 @@ void onJoystickChange(int16_t i1, int16_t i2) {
 
         motorB = -i2 + i1;
         motorA = motorB * 3 / 4;
-        if (motorB >= 1023) {
-          motorB = 1023;
+        if (motorB >= 255) {
+          motorB = 225;
         }
-        analogWrite(ENA, motorB);
-        analogWrite(ENB, motorA);
+        ledcWrite(speedmA, motorB);
+        ledcWrite(speedmB, motorA);
         
       Serial.println("traz direita");
       Serial.println(motorB);
@@ -374,8 +364,7 @@ void onRightChange(boolean b) {
 }
 void onSpeedChange(int32_t i) {
   Serial.printf("onSpeedChange: i: %d\n", i);
-  //analogWrite(ENA, i); 
-  //analogWrite(ENB, i);
+
   aceleracao = i;
 }
 
@@ -386,81 +375,67 @@ void setup() {
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(100);
-
-
   }
 
-  analogWrite(ENA, 500);
-  analogWrite(ENB, 500);
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH);
-  delay(100);
-  analogWrite(ENA, 500);
-  analogWrite(ENB, 500);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, LOW);
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, LOW);
-  delay(50);
-  analogWrite(ENA, 500);
-  analogWrite(ENB, 500);
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH);
-  delay(100);
-  analogWrite(ENA, 500);
-  analogWrite(ENB, 500);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, LOW);
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, LOW);
-  delay(50);
-  analogWrite(ENA, 500);
-  analogWrite(ENB, 500);
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH);
-  delay(100);
-  analogWrite(ENA, 500);
-  analogWrite(ENB, 500);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, LOW);
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, LOW);
-  delay(50);
-
-  analogWrite(ENA, 0);
-  analogWrite(ENB, 0);
-
-  remoteMe.getVariables() -> observeInteger("WiFi strength", onWiFi_strengthChange);
-
-  remoteMe.getVariables() -> observeSmallInteger2("Joystick", onJoystickChange);
-
-  remoteMe.getVariables() -> observeBoolean("Backward", onBackwardChange);
-  remoteMe.getVariables() -> observeBoolean("Forward", onForwardChange);
-  remoteMe.getVariables() -> observeBoolean("Left", onLeftChange);
-  remoteMe.getVariables() -> observeBoolean("Right", onRightChange);
-  remoteMe.getVariables() -> observeInteger("Speed", onSpeedChange);
+  remoteMe.getVariables()->observeBoolean("Forward" ,onForwardChange);
+  remoteMe.getVariables()->observeBoolean("Backward" ,onBackwardChange);
+  remoteMe.getVariables()->observeBoolean("Left" ,onLeftChange);
+  remoteMe.getVariables()->observeBoolean("Right" ,onRightChange);
+  remoteMe.getVariables()->observeInteger("Speed" ,onSpeedChange);
+  remoteMe.getVariables()->observeSmallInteger2("Joystick" ,onJoystickChange);
+  remoteMe.getVariables()->observeInteger("WiFi strength" ,onWiFi_strengthChange);
 
   remoteMe.setConnector(new RemoteMeSocketConnector());
   remoteMe.setDirectConnector(new RemoteMeDirectWebSocketConnector());
   remoteMe.sendRegisterDeviceMessage(DEVICE_NAME);
 
-  pinMode(ENA, OUTPUT);
-  pinMode(ENB, OUTPUT);
-
+  
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
 
-  analogWrite(ENA, 0);
-  analogWrite(ENB, 0);
+  // configure PWM functionalitites
+  ledcSetup(speedmA, freq, resolution);
+  ledcSetup(speedmB, freq, resolution);
 
+  // attach the channel to the GPIO to be controlled
+  ledcAttachPin(ENA, speedmA);
+  ledcAttachPin(ENB, speedmB);
+
+
+
+  ledcWrite(speedmA, 100);
+  ledcWrite(speedmB, 100);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  delay(100);
+  ledcWrite(speedmA, 100);
+  ledcWrite(speedmB, 100);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  delay(50);
+  ledcWrite(speedmA, 100);
+  ledcWrite(speedmB, 100);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  delay(100);
+  ledcWrite(speedmA, 100);
+  ledcWrite(speedmB, 100);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  delay(50);
+ 
+  ledcWrite(speedmA, 0);
+  ledcWrite(speedmB, 0);
 }
 
 void loop() {
@@ -496,8 +471,8 @@ void loop() {
       Serial.print("menos que 500 'rapido' ");
       Serial.println(acelerado);
 
-      analogWrite(ENA, acelerado);
-      analogWrite(ENB, acelerado);
+      ledcWrite(speedmA, acelerado);
+      ledcWrite(speedmB, acelerado);
       delay(1);
     }
 
@@ -508,8 +483,8 @@ void loop() {
       Serial.print("menos que 500 'rapido' ");
       Serial.println(acelerado);
 
-      analogWrite(ENA, acelerado);
-      analogWrite(ENB, acelerado);
+      ledcWrite(speedmA, acelerado);
+      ledcWrite(speedmB, acelerado);
       delay(1);
     }
   }
@@ -524,8 +499,8 @@ void loop() {
       Serial.print("mais0 que 500 'devagar' ");
       Serial.println(acelerado);
 
-      analogWrite(ENA, acelerado);
-      analogWrite(ENB, acelerado);
+      ledcWrite(speedmA, acelerado);
+      ledcWrite(speedmB, acelerado);
       delay(1);
     }
 
@@ -536,23 +511,22 @@ void loop() {
       Serial.print("mais0 que 500 'devagar' ");
       Serial.println(acelerado);
 
-      analogWrite(ENA, acelerado);
-      analogWrite(ENB, acelerado);
+      ledcWrite(speedmA, acelerado);
+      ledcWrite(speedmB, acelerado);
       delay(1);
     }
   }
 
-  if ((a == 0) and (-17 < Y < 17) and (-17 < X < 17) and (Y=X)) {
-    acelerado = 10;
-    delay(1);
+//  if ((a == 0) and (-17 < Y < 17) and (-17 < X < 17) and (Y=X)) {
+//    acelerado = 10;
+//    delay(1);
+//
+//    if (millis() > lastExecutedMillis2 + 500) {
+//      lastExecutedMillis2 = millis();
+//      Serial.print("Stop");
+//      Serial.println();
+//    }
+//  }
 
-    if (millis() > lastExecutedMillis2 + 500) {
-      lastExecutedMillis2 = millis();
-      Serial.print("Stop");
-      Serial.println();
-    }
-  }
-
-  /////////////////////////////////////////////////////////////////////////////////////////
 
 }
